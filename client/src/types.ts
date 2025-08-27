f1b9667136e6f507b29bfa66c9034ae8eff199d0
Eyüp Karataş <eyupkaratasbusiness@gmail.com>
2025-08-27 15:53:30 +0300

    Complete ShippingForm

--- client/src/types.ts
+++ client/src/types.ts
@@ -23,6 +23,14 @@ export type CartItemsType = CartItemType[];
 
 export const shippingFormSchema = z.object({
   name: z.string().min(1, "Name is required!"),
-  email: z.string().min(1, "Name is required!"),
-  phone: z.string().min(1, "Name is required!"),
+  email: z.string().min(1, "Email is required!"),
+  phone: z
+    .string()
+    .min(7, "Phone number must be between 7 and 10 digits!")
+    .max(10, "Phone number must be between 7 and 10 digits!")
+    .regex(/^\d+$/, "Phone number must contain only numbers"),
+  address: z.string().min(1, "Adress is required!"),
+  city: z.string().min(1, "City is required!"),
 });
+
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
