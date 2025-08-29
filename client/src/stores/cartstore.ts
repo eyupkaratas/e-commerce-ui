6226076fd03c2e7173564a9f00eca4294a09a727
Eyüp Karataş <eyupkaratasbusiness@gmail.com>
2025-08-29 19:24:43 +0300

    feat(cartstore): add hydration check and integrate in ShoppingCartIcon component

--- client/src/stores/cartstore.ts
+++ client/src/stores/cartstore.ts
@@ -6,6 +6,7 @@ const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
   persist(
     (set) => ({
       cart: [],
+      hasHydrated: false,
       addToCart: (product) =>
         set((state) => {
           const existingIndex = state.cart.findIndex(
@@ -24,13 +25,25 @@ const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
         }),
       removeFromCart: (product) =>
         set((state) => ({
-          cart: state.cart.filter((p) => p.id !== product.id),
+          cart: state.cart.filter(
+            (p) =>
+              !(
+                p.id === product.id &&
+                p.selectedSize === product.selectedSize &&
+                p.selectedColor === product.selectedColor
+              )
+          ),
         })),
       clearCart: () => set({ cart: [] }),
     }),
     {
       name: "cart",
       storage: createJSONStorage(() => localStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.hasHydrated = true;
+        }
+      },
     }
   )
 );
